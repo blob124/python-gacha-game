552f320548e44a72cd1b9ae93cf4bf894bcdabbf
Sopon Pumsawai <pruk39473@gmail.com>
2025-10-31 19:21:26 +0700

    Squashed commit of the following:
    commit 6b470e487b879d4f9ab1c1eeedfb3b3acf0ec1ec
    Author: Sopon Pumsawai <pruk39473@gmail.com>
    Date:   Fri Oct 31 19:08:19 2025 +0700
    
        demo

--- scenes/ArchivePage.py
+++ scenes/ArchivePage.py
@@ -1,27 +1,25 @@
 import pygame
-import random
 from stuff import *
 
-FONT24 = pygame.font.SysFont(None, 24)
-FONT40 = pygame.font.SysFont(None, 40)
-EXITTXT = FONT24.render('Press Esc, it stands for Escape!', True, (255, 255, 255))
+EXITTXT = pygame.font.SysFont(None, 24).render('Press Esc, it stands for Escape!', True, (255, 255, 255))
 
-class Archive:
+class Archive(Scene):
 	def __init__(page,game):
 		page.game = game
 
+		page.bg = pygame.Surface(page.game.screen.get_size())
+		page.bg.fill((30,100,30))
+		page.bg.blit(EXITTXT,(20,20))
+
 		page.images = {}
-		char_0_icon = page.game.data['Man'].getIcon(bg=True)
-		char_0_art = page.game.data['Man'].getArt()
-		
-		one_image = pygame.image.load('data/numba1.png').convert_alpha()
-		one_image = pygame.transform.scale_by(one_image, (2, 0.2))
-
-		page.images['image0'] = Image(char_0_icon,(50,75))
-		page.images['image1'] = Image(char_0_art,(650,75))
-		page.images['one'] = Image(one_image,(150,200))
-
+		page.char_icons = {}
+		page.char_arts = {}
 		page.buttons = {}
+		page.groups = {	-2:page.char_icons,
+				 		-1:page.char_arts,
+						0:page.images,
+				 		1:page.buttons}
+
 		page.buttons['buttonname'] = SimpleButton(pygame.Rect(200,450,100,50),
 			[TextBox(pygame.Rect(0,0,100,50),bgcolor=(0,255,0),text='TEXT HERE',textcolor=(247,13,26),aligncenter=True)],
 			[TextBox(pygame.Rect(0,0,100,50),bgcolor=(0,255,0),text='HOVERING',textcolor=(247,13,26),aligncenter=True)]
@@ -30,6 +28,16 @@ class Archive:
 		page.buttons['alsobuttonname'] = SimpleButton(pygame.Rect(400,450,100,50),
 			[TextBox(pygame.Rect(0,0,100,50),bgcolor=(0,190,255),text='TEXT HERE\n2',textcolor=(247,13,26),aligncenter=True)],
 			[TextBox(pygame.Rect(0,0,100,50),bgcolor=(0,144,190),text='HOVERINGING',textcolor=(247,13,26),textsize=18,aligncenter=True)])
+
+	def enter(page):
+		char_0_icon = page.game.data['Man'].getIcon(bg=True)
+		char_0_art = page.game.data['Man'].getArt()
+		one_image = pygame.image.load('data/numba1.png').convert_alpha()
+		one_image = pygame.transform.scale_by(one_image, (2, 0.2))
+
+		page.char_icons['image0'] = Image(char_0_icon,(50,75))
+		page.char_arts['image1'] = Image(char_0_art,(650,75))
+		page.images['one'] = Image(one_image,(150,200))
 
 	def handle_events(page, events):
 		for event in events:
@@ -45,17 +53,14 @@ class Archive:
 					pass
 
 	def update(page):
-		for name,button in page.buttons.items():
-			button.checkHover(page.game.mousepos)
+		for _,obj in page.buttons.items():
+			obj.state = 1 if obj.isHover(page.game.mousepos) else 0
 
 	def draw(page):
-		page.game.screen.fill((30, 100, 30))
-		page.game.screen.blit(EXITTXT, (20, 20))
+		page.game.screen.blit(page.bg,(0,0))
 
-		for id,image in page.images.items():
-			image.draw(page.game.screen,grayscale=False)
-
-		for name,button in page.buttons.items():
-			button.draw(page.game.screen)
+		for _,group in sorted(page.groups.items()):
+			for _,obj in group.items():
+				obj.draw(page.game.screen)
 		
 		pygame.draw.line(page.game.screen, (255,255,255), (650,80), (650,520), 1)
